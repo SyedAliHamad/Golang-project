e5a05b7681d8c93e34de0cfdd1ac347de61cd07b
SyedAliHamad <syedalihamad@hotmail.com>
2022-02-10 13:02:21 +0500

    Added file server for static folder

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,11 +15,14 @@ func routes(app *config.AppConfig)http.Handler{
 	mux:=chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-
 	mux.Use(NoServe)
 	mux.Use(SessionLoad)
 
 	mux.Get("/",handlers.Repo.Home)
 	mux.Get("/about",handlers.Repo.About)
+
+	fileServer:= http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static",fileServer))
+
 	return mux
 }
